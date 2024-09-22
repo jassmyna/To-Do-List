eedec63191d815440633006b374a7bd7980b1d68
Jassmyna Stetco <166325758+jassmyna@users.noreply.github.com>
2024-09-22 14:20:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+//Clock App
+let time = document.getElementById("current-time");
+
+setInterval(() =>{
+   let d = new Date();
+time.innerHTML = d.toLocaleTimeString(); 
+},1000)
+
+
+//To-Do App
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById("list-container");
+
+function addTask(){
+    if(inputBox.value === ''){
+        alert("You must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML =  "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+inputBox.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        addTask(); // Call addTask function when Enter is pressed
+    }
+});
+
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
